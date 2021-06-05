388699d94b75c842c1f2fc2387d958670b248fc1
Arthur <arthursun2005@gmail.com>
2021-06-05 15:22:08 +1000

    a

--- client/main.py
+++ client/main.py
@@ -1,6 +1,35 @@
 import pyglet
+import sys
+import requests
+from pyglet.gl import *
 
-def main():
-    pass
+display = pyglet.canvas.Display()
+screen = display.get_default_screen()
 
-main()
+window = pyglet.window.Window(screen.width - 200, screen.height - 200, caption='bonknogg', visible=True)
+
+host = sys.argv[1]
+
+def get():
+    return requests.get(f'{host}:6969').content
+
+pos = []
+
+@window.event
+def on_draw():
+    window.clear()
+
+    pyglet.graphics.draw(len(pos) // 2, GL_QUADS, (
+        'v2f', pos
+    ))
+
+def update(dt):
+    global pos
+
+    k = float(get())
+
+    pos = [k, k, k + 10, k, k + 10, k + 10, k, k + 10]
+
+pyglet.clock.schedule_interval(update, 0.016)
+pyglet.app.run()
+
--- server/main.py
+++ server/main.py
@@ -1,10 +1,42 @@
-import requests
-import Box2D
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+from Box2D import *
+import time
 import sys
 
 host = sys.argv[1]
 
+dt = 0.016
+
+world = b2World(gravity=(0, -10))
+body = world.CreateDynamicBody()
+
+circle = b2CircleShape(pos=(1, 2), radius=0.5)
+f1 = body.CreateFixture(shape=circle, density=1, friction=1, restitution=1)
+
+x = 1.0
+
+def main_game():
+    global x
+    while True:
+        x += 40.0 * dt
+        world.Step(dt, 3, 3)
+        time.sleep(dt)
+
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        self.wfile.write(f'{x}'.encode('utf-8'))
+
 def main():
-    pass
+    game = threading.Thread(target=main_game)
+    game.start()
+
+    A = HTTPServer((host, 6969), Server)
+    print('READY')
+    A.serve_forever()
 
 main()

f3b59112a4baae80dded4a745dd467ab9302c2ca
Jerry Li <zirui.li7@gmail.com>
2021-06-06 23:04:38 +1000

    3

--- client/main.py
+++ client/main.py
@@ -1,4 +1,5 @@
 import pyglet
+import numpy as np
 import pyglet.window.key as key
 import sys
 import requests
@@ -8,15 +9,29 @@ from pyglet.gl import *
 display = pyglet.canvas.Display()
 screen = display.get_default_screen()
 
-window = pyglet.window.Window(screen.width, screen.height, caption='bonknogg', fullscreen=True, visible=True)
+window = pyglet.window.Window(1280, 720, caption='bonknogg', visible=True)
 keys = key.KeyStateHandler()
 window.push_handlers(keys)
 #window.set_exclusive_mouse(True)
+batch = pyglet.graphics.Batch()
 
 host = sys.argv[1]
 
+options = {
+        "U":key.UP,
+        "D":key.DOWN,
+        "L":key.LEFT,
+        "R":key.RIGHT,
+        "X":key.X,
+        "C":key.C,
+        "Z":key.Z
+        }
+
 def get():
-    return requests.get(f'{host}:6969',headers={'X': json.dumps(keys)}).content
+    tem = {}
+    for i in options:
+        tem[i]=keys[options[i]]
+    return requests.get(f'{host}:6969',headers={'X': json.dumps(tem)}).content
 
 pos = []
 
@@ -24,16 +39,24 @@ pos = []
 def on_draw():
     window.clear()
 
-    pyglet.graphics.draw(len(pos) // 2, GL_QUADS, (
+    pyglet.graphics.draw(len(pos) // 2, GL_POLYGON, (
         'v2f', pos
     ))
 
 def update(dt):
     global pos
 
-    k = float(get())
-
-    pos = [k, k, k + 10, k, k + 10, k + 10, k, k + 10]
+    temp=get().decode("utf-8")
+    if temp!="":
+        px,py = map(float, temp.split())
+        px*=100
+        py*=100
+        print(f'{px} {py}')
+        pos = []
+        for a in range(0,30):
+            v = 2 * np.pi / 30 * a
+            pos.append(px+np.cos(v)*20)
+            pos.append(py+np.sin(v)*20)
 
 pyglet.clock.schedule_interval(update, 0.016)
 pyglet.app.run()
--- server/main.py
+++ server/main.py
@@ -3,34 +3,57 @@ import threading
 from Box2D import *
 import time
 import sys
+import json
 
 host = sys.argv[1]
 
 dt = 0.016
 
 world = b2World(gravity=(0, -10))
-body = world.CreateDynamicBody()
 
-circle = b2CircleShape(pos=(1, 2), radius=0.5)
-f1 = body.CreateFixture(shape=circle, density=1, friction=1, restitution=1)
+p1 = world.CreateDynamicBody(position=(6.4,7.2))
+p1f = p1.CreateFixture(
+        shape=b2CircleShape(pos=(0, 0), radius=0.2),
+        density=7.95775387622, friction=1, restitution=0.25
+        )
 
-x = 1.0
+ground = world.CreateStaticBody(
+        position=(6.4,-1),
+        shapes=b2PolygonShape(box=(6.4,1)),
+        )
+
+acc=0
 
 def main_game():
-    global x
     while True:
-        x += 40.0 * dt
-        world.Step(dt, 3, 3)
+        last=p1.linearVelocity.y
+        world.Step(dt, 5, 5)
+        acc=p1.linearVelocity.y-last
         time.sleep(dt)
+        print(f'{p1.position.x} {p1.position.y}:{acc}')
 
 class Server(BaseHTTPRequestHandler):
+    jmplast=False
     def do_GET(self):
         print(self.headers)
+        tem = json.loads(self.headers['X'])
+        if tem["L"]:
+            p1.ApplyForce(force=(-5,0),point=p1.position,wake=True)
+        if tem["R"]:
+            p1.ApplyForce(force=(5,0),point=p1.position,wake=True)
+        if tem["D"]:
+            p1.ApplyForce(force=(0,-5),point=p1.position,wake=True)
+        if tem["U"]:
+            if acc > -10*dt+0.0001 and acc < 0.0001 and not jmplast:
+                p1.ApplyForce(force=(0,15),point=p1.position,wake=True)
+                jmplast=True
+            else:
+                jmplast=False
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
-        self.wfile.write(f'{x}'.encode('utf-8'))
+        self.wfile.write(f'{p1.position.x} {p1.position.y}'.encode('utf-8'))
 
 def main():
     game = threading.Thread(target=main_game)

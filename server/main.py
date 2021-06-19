fb706b1c3e1454d9618773f1f9710a93dfbf617b
Arthur <arthursun2005@gmail.com>
2021-06-19 13:16:32 +1000

    gitgud

--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
+import socketserver
 import threading
-import socket
 import time
 import sys
 import json
@@ -13,20 +13,19 @@ def main_game():
     while True:
         wm.step()
 
+class Server(socketserver.BaseRequestHandler):
+    def handle(self):
+        #self.request.sendall("test".encode('utf-8'))
+        while True:
+            response = wm.parse(json.loads(self.request.recv(1024).decode('utf-8')))
+            self.request.sendall(response.encode('utf-8'))
+
 def main():
     game = threading.Thread(target=main_game)
     game.start()
 
-    print('Ready')
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, 6969))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                response = wm.parse(json.loads(conn.recv(1024).decode('utf-8')))
-                conn.sendall(response.encode('utf-8'))
+    with socketserver.TCPServer((host, 6969), Server) as A:
+        print('READY')
+        A.serve_forever()
 
 main()

f4c365e70588dd57a6e94cfc8254928ba0ed825c
Jerry Li <zirui.li7@gmail.com>
2021-06-20 14:04:01 +1000

    fuck my life

--- client/main.py
+++ client/main.py
@@ -7,15 +7,22 @@ import requests
 import json
 import random
 import string
-from pyglet.gl import *
+import pygame
+from pygame.locals import *
+clock = pygame.time.Clock()
+#from pyglet.gl import *
 
-display = pyglet.canvas.Display()
-screen = display.get_default_screen()
+#display = pyglet.canvas.Display()
+#screen = display.get_default_screen()
 
-window = pyglet.window.Window(1440, 860, caption='bonknogg', visible=True)
+#window = pyglet.window.Window(1440, 860, caption='bonknogg', visible=True)
 keys = key.KeyStateHandler()
-window.push_handlers(keys)
-batch = pyglet.graphics.Batch()
+#window.push_handlers(keys)
+#batch = pyglet.graphics.Batch()
+
+pygame.init()
+screen = pygame.display.set_mode([1440, 860], #pygame.FULLSCREEN, 
+        )
 
 host = sys.argv[1]
 
@@ -51,15 +58,15 @@ def get():
 polygons = []
 cols = []
 
-@window.event
-def on_draw():
-    window.clear()
+#@window.event
+#def on_draw():
+    #window.clear()
 
-    for i, p in enumerate(polygons):
-        c = cols[i]
-        pyglet.graphics.draw(len(p) // 2, GL_POLYGON,
-            ('v2f', p), ('c3f', c)
-        )
+    #for i, p in enumerate(polygons):
+        #c = cols[i]
+        #pyglet.graphics.draw(len(p) // 2, GL_POLYGON,
+            #('v2f', p), ('c3f', c)
+        #)
 
 def update(dt):
     global polygons
@@ -68,23 +75,26 @@ def update(dt):
     scl = 80
     temp=get()
     if len(temp) == 0:
-       return 
+        print("ohno")
+        return 
     obj = json.loads(temp.decode('utf-8'))
     ptr = 0
     vs = list(map(float, obj['vs']))
+    print(vs)
 
     polygons = []
     cols = []
 
     dashes = int(obj['sz'][0])
 
-    polygons.append([0, window.height - 50, 0, window.height, window.width, window.height, window.width, window.height - 50])
+    width, height = pygame.display.get_surface().get_size()
+    polygons.append([(0, height - 50), (0, height), (width, height), (width, height - 50)])
     if dashes == 0:
-        cols.append([0.5, 1.0, 1.0] * 4)
+        cols.append((127, 255, 255))
     elif dashes == 1:
-        cols.append([0.9, 0.2, 0.2] * 4)
+        cols.append((230, 51, 51))
     else:
-        cols.append([0.9, 0.6, 1.0] * 4)
+        cols.append((230, 153, 255))
 
     for s in obj['sz'][1:]:
         sz = int(s)
@@ -96,20 +106,40 @@ def update(dt):
 
             for a in range(0, 20):
                 v = 2 * np.pi / 20 * a
-                w.append(px*scl+np.cos(v)*r*scl+window.width / 2)
-                w.append(py*scl+np.sin(v)*r*scl)
-                c.extend(vs[ptr + 3:ptr + 6])
+                w.append((px*scl+np.cos(v)*r*scl+width / 2, 
+                    py*scl+np.sin(v)*r*scl))
+            c=tuple(vs[ptr + 3:ptr + 6])
             ptr += 6
         else:
             for i in range(sz):
-                x = scl * vs[ptr] + window.width / 2
+                x = scl * vs[ptr] + width / 2
                 y = scl * vs[ptr + 1]
-                w.extend([x, y])
-                c.extend(vs[ptr + 2:ptr + 5])
+                w.append((x, y))
                 ptr += 5
+            c=tuple(vs[ptr + 2:ptr + 5])
         polygons.append(w)
         cols.append(c)
 
-pyglet.clock.schedule_interval(update, 1/60)
-pyglet.app.run()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    update(1/60)
+    screen.fill((255,255,255))
+    for i, p in enumerate(polygons):
+        #c = cols[i]
+        #pyglet.graphics.draw(len(p) // 2, GL_POLYGON,
+            #('v2f', p), ('c3f', c):w
+
+        #)
+        print(cols[i])
+        print(p)
+        pygame.draw.polygon(surface=screen, color=cols[i], points=p)
+    screen.blit(pygame.transform.flip(screen,True,False),(0,0))
+    pygame.display.flip()
+    clock.tick(60)
+#pyglet.clock.schedule_interval(update, 1/60)
+#pyglet.app.run()
 
